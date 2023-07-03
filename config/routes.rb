6e4396cfc4b576ab63edbd1ad16938799e53d73a
Baigv25 <baigv25@gmail.com>
2023-07-03 15:54:19 -0600

    Devise fixes 2

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :clients
     resources :plates
     resources :orders
+    resources :users
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
